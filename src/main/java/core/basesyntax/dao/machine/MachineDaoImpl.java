5e91f7f281f92b8e17ae07adb6389bb74241752a
testeracc <testeraccmail8@gmail.com>
2025-07-17 16:17:34 +0300

    [HIBERNATE-INHERITANCE] implement methods in DAO

--- src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
+++ src/main/java/core/basesyntax/dao/machine/MachineDaoImpl.java
@@ -1,9 +1,15 @@
 package core.basesyntax.dao.machine;
 
 import core.basesyntax.dao.AbstractDao;
+import core.basesyntax.model.machine.Car;
 import core.basesyntax.model.machine.Machine;
+import core.basesyntax.model.machine.Truck;
+import java.time.Year;
+import java.util.ArrayList;
 import java.util.List;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class MachineDaoImpl extends AbstractDao implements MachineDao {
     public MachineDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +18,41 @@ public class MachineDaoImpl extends AbstractDao implements MachineDao {
 
     @Override
     public Machine save(Machine machine) {
-        return null;
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(machine);
+            transaction.commit();
+            return machine;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't save machine " + machine, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Machine> findByAgeOlderThan(int age) {
-        return null;
+        int currentYear = Year.now().getValue();
+        int maxYear = currentYear - age;
+        try (Session session = sessionFactory.openSession()) {
+            List<Machine> result = new ArrayList<>();
+            result.addAll(session.createQuery("FROM Car c WHERE c.year < :maxYear", Machine.class)
+                    .setParameter("maxYear", maxYear)
+                    .getResultList());
+            result.addAll(session.createQuery("FROM Truck t WHERE t.year < :maxYear", Machine.class)
+                    .setParameter("maxYear", maxYear)
+                    .getResultList());
+            return result;
+        } catch (Exception e) {
+            throw new RuntimeException("Can't find machines older than " + age + " years", e);
+        }
     }
 }
